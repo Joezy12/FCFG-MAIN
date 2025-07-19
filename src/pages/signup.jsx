39a53d8e962a1ad5c1898b72d2cc03e588480431
Joezy12 <oyebodejoseph74@gmail.com>
2025-07-19 04:31:50 -0700

    inito

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,17 +1,24 @@
 import { NavLink } from "react-router-dom";
 import SecondNavbar from "../general/secondNavbar";
 import '../styles/signup.css'
+import { useState } from "react";
 
 function SignUp() {
-    return (
-        <>
-        <SecondNavbar />
-        <div className="signup">
-          <div className="sign-head">
-            <p>Get started by telling us about you</p>
-            <span>To set up online access we need to locate you in our system</span>
-          </div>
-          <form className="signup-form">
+
+    const [isSetup, setIsSetup] = useState("personal")
+
+    
+    function noSubmit(e) {
+     e.preventDefault();
+      setIsSetup("loading")
+        setTimeout(()=> {
+            finish();
+        }, 1500)
+    }
+
+    function signState(){
+        if (isSetup == "personal") {
+            return <form className="signup-form" onSubmit={noSubmit}>
              <p className="span">Enter your personal information</p>
              <div className="sign-input">
                <p>Full Name</p>
@@ -27,6 +34,44 @@ function SignUp() {
              </div>
              <button>Get Started</button>
           </form>
+        }else if(isSetup == "loading") {
+            return <div className="loading">
+                <span class="loader"></span>
+            </div>
+        }else if(isSetup == "finish") {
+            return <form className="signup-form">
+             <p className="span">Finish setting up your account</p>
+             <div className="sign-input">
+               <p>Email Address</p>
+               <input type="text" />
+             </div>
+              <div className="sign-input">
+               <p>Password</p>
+               <input type="number" placeholder="000000000"/>
+             </div>
+             <div className="sign-input">
+               <p>Confirm Password</p>
+               <input type="date" />
+             </div>
+             <button>Finish Up</button>
+          </form>
+        }
+    }
+
+    function finish() {
+        setIsSetup("finish")
+    }
+
+ 
+    return (
+        <>
+        <SecondNavbar />
+        <div className="signup">
+          <div className="sign-head">
+            <p>Get started by telling us about you</p>
+            <span>To set up online access we need to locate you in our system</span>
+          </div>
+          {signState()}
           <NavLink className="link" to="../login"><p className="already">Already have an account? Sign In</p></NavLink>
         </div>
         </>
