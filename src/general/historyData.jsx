aeb29c2e3a7162b4766a25c2f15634965b253cd9
Joezy12 <oyebodejoseph74@gmail.com>
2025-07-24 08:00:15 -0700

    newer

--- src/general/historyData.jsx
+++ src/general/historyData.jsx
@@ -6,11 +6,32 @@
         date: "jun 01, 2025",
         topic: "interest earned",
         amount: "0.02",
+        type: true,
     },
      {
-        date: "jun 01, 2025",
-        topic: "interest earned",
+        date: "may 28, 2025",
+        topic: "CAPITAL ONE CARD PYMT, RICHMON..",
+        amount: "4.00",
+        type: false,
+    },
+      {
+        date: "may 21, 2025",
+        topic: "Transfer from Savaings to Dave Che..",
+        amount: "0.83",
+        type:true,
+
+    },
+      {
+        date: "may 09, 2025",
+        topic: "Transfer from Savaings to Dave Che..",
+        amount: "0.83",
+        type: false,
+    },
+      {
+        date: "may 28, 2025",
+        topic: "Transfer from Savaings to Dave Che..",
         amount: "0.02",
+        type: true,
     },
 ]
 
--- src/pages/checkingDetails.jsx
+++ src/pages/checkingDetails.jsx
@@ -5,11 +5,24 @@ import { NavLink } from "react-router-dom";
 import historyData from "../general/historyData";
 
 function CheckingDetails() {
-  console.log(historyData)
+    console.log(historyData)
+
+    const showHistory = historyData.map((each) => {
+        return <div className="recent-line">
+            <div className="r-left">
+                <p>{each.date}</p>
+                <h3>{each.topic.slice(0, 22)} ..</h3>
+            </div>
+            <div className="r-right">
+                {each.type ? <p className="credit">+${each.amount}</p> : <p>${each.amount}</p>}
+            </div>
+        </div>
+    })
+
     return (
         <>
             <div className="check-nav">
-              <NavLink className="link" to="../homeDash"><i className="bi-arrow-left"></i></NavLink>  
+                <NavLink className="link" to="../homeDash"><i className="bi-arrow-left"></i></NavLink>
                 <h1>FCFG Checking</h1>
                 <i className="bi-gear"></i>
             </div>
@@ -20,11 +33,11 @@ function CheckingDetails() {
                             <p>Account Balance</p>
                             <h1>$200.02</h1>
                             <p>4.00% APY</p>
-                             <h3>**** **** **** 0237</h3>
+                            <h3>**** **** **** 0237</h3>
                         </div>
                         <div className="acc-balance-right">
                             <p>Debit Card</p>
-                           
+
                         </div>
                     </div>
                     <div className="add-money">
@@ -35,9 +48,47 @@ function CheckingDetails() {
             </div>
 
             <div className="recent">
-
+                <h1>Recent activity <i className="bi-arrow-right"></i></h1>
+                <div className="recent-box">
+                    {showHistory}
+                </div>
             </div>
-            <BottomNav homeState="home"/>
+
+
+            <div className="last-do">
+                <section className="do-more">
+                    <h1>Do more with FCFG Checking</h1>
+                    <div className="do-more-container">
+                        <div className="do-more-box">
+                            <i className="bi-cash"></i>
+                            <p>Direct <br /> Deposit</p>
+                        </div>
+                        <div className="do-more-box">
+                            <i className="bi-arrow-left-right"></i>
+                            <p>Recurring transfer</p>
+                        </div>
+                        <div className="do-more-box">
+                            <i className="bi-envelope-paper"></i>
+                            <p>Scan or send<br /> a check</p>
+                        </div>
+                    </div>
+                    <div className="do-more-container">
+                        <div className="do-more-box">
+                            <i className="bi-cash-coin"></i>
+                            <p>Deposit <br /> Cash</p>
+                        </div>
+                        <div className="do-more-box">
+                            <i className="bi-printer-fill"></i>
+                            <p>Withdraw <br /> cash</p>
+                        </div>
+                        <div className="do-more-box">
+                            <i className="bi-envelope-paper"></i>
+                            <p>Pay <br /> bills</p>
+                        </div>
+                    </div>
+                </section>
+            </div>
+            <BottomNav homeState="home" />
         </>
     )
 }
