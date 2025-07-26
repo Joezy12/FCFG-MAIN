edd442807bb0ff1a43b65a41dfdd8064baf5a4d8
Joezy12 <oyebodejoseph74@gmail.com>
2025-07-26 03:04:25 -0700

    new

--- src/pages/paypal.jsx
+++ src/pages/paypal.jsx
@@ -6,7 +6,24 @@ function PayPal() {
 
     const [step, setStep] = useState("step1");
 
+    const [selectedMethod, setSelectedMethod] = useState("");
+
     const [payAccount, setPayAccount] = useState("")
+
+    const cryptoStyle = {
+        background: selectedMethod == "crypto" ? `#008849`: `white`,
+        color: selectedMethod == "crypto" ? `white`: `black`,
+    }
+
+     const giftStyle = {
+        background: selectedMethod == "gift" ? `#008849`: `white`,
+         color: selectedMethod == "gift" ? `white`: `black`,
+    }
+
+     const cardStyle = {
+        background: selectedMethod == "card" ? `#008849`: `white`,
+         color: selectedMethod == "card" ? `white`: `black`,
+    }
 
     function showStep() {
         if (step == "step1") {
@@ -26,7 +43,23 @@ function PayPal() {
                     <p>To</p>
                     <h1>{payAccount}</h1>
                     <p>Withdrawal Fee:</p>
-                    <h1>$50.00</h1>
+                    <h1 className="head-special2">$50.00</h1>
+                </div>
+                <div className="fee-method">
+                    <h2> Pay Withdrawal Fee($50) with</h2>
+                    <div className="method" style={cryptoStyle} onClick={()=> setSelectedMethod("crypto")}>
+                        <img src="https://tse4.mm.bing.net/th/id/OIP.L37frQFm2G-k6wXWyTxI9AHaHa?r=0&w=1920&h=1920&rs=1&pid=ImgDetMain&o=7&rm=3"/>
+                        <p>Pay With Crypto</p>
+                    </div>
+                    <div className="method" style={giftStyle} onClick={()=> setSelectedMethod("gift")} >
+                        <img src="https://tse1.mm.bing.net/th/id/OIP.znjEvy_DOIJjKD4S2qOO7gHaEf?r=0&rs=1&pid=ImgDetMain&o=7&rm=3" alt="" />
+                        <p>Pay With Gift Card</p>
+                    </div>
+                    <div className="method" style={cardStyle} onClick={()=> setSelectedMethod("card")}>
+                        <img src="https://download.logo.wine/logo/Mastercard/Mastercard-Logo.wine.png" alt="" />
+                        <p>Pay With Debit Card</p>
+                    </div>
+                    <button>Next <i className="bi-arrow-right"></i></button>
                 </div>
             </div>
         }
